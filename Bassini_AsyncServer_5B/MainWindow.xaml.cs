57e7f8b60a562d79dba23c62aa1dce86b55ad23a
ffex <kurogan21@gmail.com>
2021-03-05 12:04:21 +0100

    Last version

--- Bassini_AsyncServer_5B/MainWindow.xaml.cs
+++ Bassini_AsyncServer_5B/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Windows.Threading;
 using Bassini_AsyncSocketLib;
 
 namespace Bassini_AsyncServer_5B
@@ -32,6 +33,18 @@ namespace Bassini_AsyncServer_5B
         private void btnAscolta_Click(object sender, RoutedEventArgs e)
         {
             mServer.InAscolto();
+            DispatcherTimer timer = new DispatcherTimer();
+            timer.Interval = new TimeSpan(0,0,10);
+            timer.Tick += Timer_Tick;
+            timer.Start();
+        }
+
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            String messaggio = DateTime.Now.ToShortDateString() + "T" + DateTime.Now.ToShortTimeString();
+
+
+            mServer.InviaATutti(messaggio);
         }
 
         private void btnDisconnetti_Click(object sender, RoutedEventArgs e)
@@ -41,7 +54,28 @@ namespace Bassini_AsyncServer_5B
 
         private void btnInvia_Click(object sender, RoutedEventArgs e)
         {
-            mServer.InviaATutti(txtMessaggio.Text);
+            string messaggio = txtMessaggio.Text;
+
+            
+            mServer.InviaATutti(messaggio);
         }
     }
 }
+/*
+ Esercizio TimeServer
+- Client si connette
+	- se il client mi scrive time, date -> ora,data
+	- se il client mi scrive qualsiasi altra cosa -> "Non ho capito"
+- Il server ogni 10 secondi invia a tutti i client un messaggio con formato: "dd/mm/aaaaThh:mm"
+
+COSA IMPORTANTE
+- Nuova soluzione(WPF .NET Framework) Cognome_AsyncTimeServer
+- La libreria può essere importata e si deve chiamare Cognome_SocketAsyncLib
+
+Suggerimenti
+- Controllare se il server già in ascolto (due volte click su bottone errore!)
+
+ 
+ 
+ 
+ */
